62cb59bbf1f3372f1e1615a7fa6a75b9122af2db
Adrian Romero <adrianromerozamora@hotmail.es>
2024-06-06 21:09:03 +0200

    front-pages-exercise

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,16 +1,33 @@
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ExerciseComponent } from './pages/exercise/exercise.component';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
+import { HistorialComponent } from './pages/historial/historial.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExerciseComponent,
+    FavoriteComponent,
+    HistorialComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/src/app/pages/exercise/exercise.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-exercise',
+  templateUrl: './exercise.component.html',
+  styleUrls: ['./exercise.component.scss']  
+})
+export class ExerciseComponent {
+
+}
