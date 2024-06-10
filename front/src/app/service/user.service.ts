7de4f2c97b37a86417b27037d338232e4c2e775e
Juan Manuel García del Toro <juanmag.toro@gmail.com>
2024-06-10 16:52:11 +0200

    no se que es

--- front/src/app/service/user.service.ts
+++ front/src/app/service/user.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class UserService {
-  private baseUrl = 'http://localhost:4200';
+  private baseUrl = 'http://localhost:4500';
 
   constructor(private http: HttpClient) {}
 
