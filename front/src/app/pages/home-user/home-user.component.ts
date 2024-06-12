b2a08a9f5a2f3ffb98c359c7fa9048515734b171
Juan Manuel García del Toro <juanmag.toro@gmail.com>
2024-06-12 18:38:47 +0200

    Componente home-user iniciado

---
+++ front/src/app/pages/home-user/home-user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-user',
+  templateUrl: './home-user.component.html',
+  styleUrls: ['./home-user.component.scss']
+})
+export class HomeUserComponent {
+
+}
